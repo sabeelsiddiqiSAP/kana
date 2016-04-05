2d89faf457d7a603872445c2b9c06e96e8930217
sabeelsiddiqiSAP <sabeel.siddiqi@sap.com>
2016-04-05 10:49:59 -0700

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+    def home
+    end
+    
+    def customer
+    end
+
+    def restaurant
+    end
+
+    def organization
+    end
+
+end
